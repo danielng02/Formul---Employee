0a9227535e75c8ad9a3c52856c1eccec8dfc4d58
Dan nguyen <danek.nguyen@gmail.com>
2021-03-28 21:22:05 +0200

    Přidejte soubory projektu.

---
+++ formular xaml/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace formular_xaml
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    class Person
+    {
+        public string name;
+        public string surname;
+        public int yearBirth;
+
+        public Person() { }
+        public Person(string Name, string Surname, int year)
+        {
+            name = Name;
+            surname = Surname;
+            yearBirth = year;
+        }
+    }
+
+    class Employee : Person
+    {
+        public enum education { zakladni, stredni, vyssi }
+        public education vzdelani;
+        public string job;
+        public int salary;
+        public Employee() { }
+        public Employee(string name, string surname, int year, education Education, string Job, int Salary) : base(name, surname, year)
+        {
+            vzdelani = Education;
+            job = Job;
+            salary = Salary;
+        }
+    }
+}
